6b5d08a64f12802a36cd77f6c23a44764725c7c6
Divya-Abjima <121873267+Divya-Abjima@users.noreply.github.com>
2023-04-04 16:03:45 +0530

    Add files via upload

---
+++ ConsoleApp1/Class1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ConsoleApp1
+{
+    public class Class1
+    {
+        public static void MyTasks(int i)
+        {
+            switch (i)
+            {
+                case 1:
+                    Fibonacci();
+                    break;
+                case 2:
+                    PrimeNumber();
+                    break;
+                default:
+                    Console.WriteLine("Enter a valid number");
+                    break;
+            }
+        }
+        public static void PrimeNumber()
+        {
+            Console.WriteLine("Enter the number you wish to check: ");
+            int num = Convert.ToInt32(Console.ReadLine());
+            bool isPrime = false;
+            if (num == 0 || num == 1)
+            {
+                Console.WriteLine("Not a prime number");
+            }
+            else if (num == 2)
+            {
+                Console.WriteLine(num + " is a prime number.");
+            }
+            else if (num > 2)
+            {
+                for (int i = 2; i <= num / 2; i++)
+                {
+                    if (num % i == 0)
+                    {
+                        Console.WriteLine("Not a prime number");
+                        isPrime = false;
+                        break;
+                    }
+                    else
+                    {
+                        isPrime = true;
+                    }
+                }
+                if (isPrime == true)
+                {
+                    Console.WriteLine(num + " is a prime number.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Enter a valid number.");
+            }
+        }
+
+        public static void Fibonacci()
+        {
+
+        }
+    }
+}
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,6 +1,7 @@
-﻿namespace ConsoleApp1
+﻿
+namespace ConsoleApp1
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
@@ -24,47 +25,11 @@
             }
             int idx = Convert.ToInt32(Console.ReadLine());
             //string tsk = Console.ReadLine();
-            MyTasks(idx);
+            Class1.MyTasks(idx);
         }
 
-        static void MyTasks(int i)
-        {
-            switch (i)
-            {
-                case 1 :
-                    break;
-                case 2 : PrimeNumber();
-                    break;
-                default: Console.WriteLine("Enter a valid number");
-                    break;
-            }
-        }
+       
 
-        static void PrimeNumber()
-        {
-            Console.WriteLine("Enter the number you wish to check: ");
-            int num = Convert.ToInt32(Console.ReadLine());
-            if(num<0 || num ==0|| num==1)
-            {
-                Console.WriteLine("Not a prime number");
-            }
-            else if (num > 1)
-            {
-                for(int i=2; i<= num/2; i++)
-                {
-                    var result = num % i;
-                    if(result==0)
-                    {
-                        Console.WriteLine("Not a prime number");
-                        break;
-                    }
-                }
-                Console.WriteLine(num+" is a prime number");
-            }
-            else
-            {
-                Console.WriteLine("Enter a valid number");
-            }
-        }
+        
     }
 }
